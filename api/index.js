1c6157c7c74a9415299399c03db551022eeda14f
MARIONNEAU Antoine <antoine.marionneau@aprilmarine.com>
2023-05-26 15:08:31 +0200

    initial commit

---
+++ api/index.js
@@ -0,0 +1,30 @@
+// Vars
+const dotenv = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+// Express App
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Select the env file
+dotenv.config();
+const PORT = process.env.API_PORT || 5000;
+
+// Models
+require("./src/sync")();
+
+// Swagger
+require("./src/swagger")(app);
+
+
+// Routes
+require("./src/routes/auth.routes")(app);
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`[EXPRESS] - Server listening on port ${PORT}`);
+});
---
+++ readme.md
@@ -0,0 +1,34 @@
+# NAAPP Back-End
+
+## What's in it for me ?
+
+- MariaDB Database
+- A Adminer instance to manage the DB
+- An Express Api
+- Nginx based Web Server
+
+# Installation guide
+
+## What you need
+
+| Runtime / Software |
+| ------------------ |
+| Docker             |
+| Docker Compose     |
+
+## Create a .env file following the .env.dist template
+
+## Start the containers with Docker
+
+```sh
+docker-compose up --build
+```
+# You can access to :
+- The API on :
+```sh
+localhost:5000/api
+```
+- Adminer on (Env value to log in):
+```sh
+localhost:8080
+```
